29781c2116be22cabb745b6ffaee640bbb37233d
kapedalex <72311866+kapedalex@users.noreply.github.com>
2021-12-14 21:00:03 +0300

    PostOrder
    Created post order traversal.

---
+++ BinaryTree_PostOrderTraversal/PostOrderTraversal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryTree_PostOrderTraversal
+{
+    public class PostOrderTraversal
+    {
+        public IEnumerable<int> Traversal(Node node)
+        {
+            var result = new List<int>();
+            var stack = new Stack<Node>();
+            Node lastNodeVisited = null;
+
+            while (stack.Count > 0 || node != null)
+            {
+                if (node != null)
+                {
+                    stack.Push(node);
+                    node = node.Left;
+                }
+                else
+                {
+                    var peekNode = stack.Peek();
+                    if (peekNode.Right != null && lastNodeVisited != peekNode.Right)
+                    {
+                        node = peekNode.Right;
+                    }
+                    else
+                    {
+                        result.Add(peekNode.Value);
+                        lastNodeVisited = stack.Pop();
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
