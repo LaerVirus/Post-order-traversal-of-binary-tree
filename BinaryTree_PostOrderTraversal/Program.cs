65dad983915e596570d0b54f473fa2764d8a5aba
LaerVirus <45294745+LaerVirus@users.noreply.github.com>
2021-12-14 21:06:14 +0300

    add console (#3)

--- BinaryTree_PostOrderTraversal/Program.cs
+++ BinaryTree_PostOrderTraversal/Program.cs
@@ -2,10 +2,26 @@
 
 namespace BinaryTree_PostOrderTraversal
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        static BinaryTree binaryTree = new BinaryTree();
+
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Количество вершин:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Вершины:");
+            for (int i = 0; i < n; i++)
+            {
+                binaryTree.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine("Готовое дерево:");
+            binaryTree.PrintAsTree();
+
+            Console.WriteLine("Обратный обход:");
+            BinaryTreeConsole.PrintToConsole(binaryTree);
         }
     }
 }
